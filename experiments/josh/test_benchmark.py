de677ed56ee3060082649f52ece206a28c1beb69
Josh <joshkimsj@gmail.com>
2024-04-07 02:10:01 -0400

    added bench mark and paralell benchmark

--- experiments/josh/test_benchmark.py
+++ experiments/josh/test_benchmark.py
@@ -11,88 +11,95 @@ from src.Ext4VmasTask import Ext4VmasTask
 from src.lstm_model import LSTMMlpConfig
 from src.custom_benchmark import Benchmark
 from src.MappoDecoder import MappoDecoderConfig
+import torch.multiprocessing as mp
 
 from utils.pt_export import convert_pt_to_gif
 import torch
 import os
 import time
 
-start_time = time.time()
+def main():
+    start_time = time.time()
+    seeds = { 69 }
+    # Loads from "benchmarl/conf/experiment/base_experiment.yaml"
+    experiment_config = ExperimentConfig.get_from_yaml()
 
-seeds = { 69 }
-# Loads from "benchmarl/conf/experiment/base_experiment.yaml"
-experiment_config = ExperimentConfig.get_from_yaml()
+    # Loads from "benchmarl/conf/task/vmas/balance.yaml"
+    # task = Ext1VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml()
+    tasks = [
+        BaselineVmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml(), 
+        Ext1VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml(), 
+        Ext2VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml(), 
+        Ext3VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml(), 
+        Ext4VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml()
+    ]
+    # task = Ext1VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml()
+    # print(task)
 
-# Loads from "benchmarl/conf/task/vmas/balance.yaml"
-# task = Ext1VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml()
-tasks = [
-    BaselineVmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml(), 
-    Ext1VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml(), 
-    Ext2VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml(), 
-    Ext3VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml(), 
-    Ext4VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml()
-]
-# task = Ext1VmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml()
-# print(task)
+    # Loads from "benchmarl/conf/algorithm/mappo.yaml"
+    algorithm_config_path = os.path.join('src', 'mappodecoder.yaml')
+    # algorithm_config = MappoDecoderConfig.get_from_yaml(algorithm_config_path)
+    # algorithm_config = MappoConfig.get_from_yaml()
+    algorithm_configs = [MappoConfig.get_from_yaml(), MappoDecoderConfig.get_from_yaml(algorithm_config_path)]
+    # algorithm_config = MaddpgConfig.get_from_yaml()
 
-# Loads from "benchmarl/conf/algorithm/mappo.yaml"
-algorithm_config_path = os.path.join('src', 'mappodecoder.yaml')
-# algorithm_config = MappoDecoderConfig.get_from_yaml(algorithm_config_path)
-# algorithm_config = MappoConfig.get_from_yaml()
-algorithm_configs = [MappoConfig.get_from_yaml(), MappoDecoderConfig.get_from_yaml(algorithm_config_path)]
-# algorithm_config = MaddpgConfig.get_from_yaml()
+    # Loads from "benchmarl/conf/model/layers/mlp.yaml"
+    # model_config = MlpConfig.get_from_yaml()
+    model_config_path = os.path.join('src', 'lstmmlp.yaml')
+    # model_config = LSTMMlpConfig.get_from_yaml(model_config_path)
+    # critic_model_config = MlpConfig.get_from_yaml()
+    model_configs = [MlpConfig.get_from_yaml(), LSTMMlpConfig.get_from_yaml(model_config_path)]
 
-# Loads from "benchmarl/conf/model/layers/mlp.yaml"
-# model_config = MlpConfig.get_from_yaml()
-model_config_path = os.path.join('src', 'lstmmlp.yaml')
-# model_config = LSTMMlpConfig.get_from_yaml(model_config_path)
-# critic_model_config = MlpConfig.get_from_yaml()
-model_configs = [MlpConfig.get_from_yaml(), LSTMMlpConfig.get_from_yaml(model_config_path)]
+    # THIS IS VERY IMPORTANT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    experiment_config.on_policy_collected_frames_per_batch = 1_000
+    experiment_config.off_policy_collected_frames_per_batch = 1_000
+    experiment_config.evaluation = True
+    experiment_config.render = True
+    # experiment_config.evaluation_interval = 12_000
+    experiment_config.evaluation_interval = 2000
+    experiment_config.evaluation_episodes = 10
 
-# THIS IS VERY IMPORTANT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-experiment_config.on_policy_collected_frames_per_batch = 1_000
-experiment_config.off_policy_collected_frames_per_batch = 1_000
-experiment_config.evaluation = True
-experiment_config.render = True
-# experiment_config.evaluation_interval = 12_000
-experiment_config.evaluation_interval = 2000
-experiment_config.evaluation_episodes = 10
+    experiment_config.max_n_iters = 2 # epoch
+    experiment_config.max_n_frames = 8_000_000
+    experiment_config.loggers = ["csv"]
+    experiment_config.create_json = True
+    experiment_config.save_folder = "results"
+    if torch.cuda.is_available():
+        print('running cuda')
+        experiment_config.sampling_device = 'cuda'
+        experiment_config.train_device = 'cuda'
+    experiment_config.off_policy_train_batch_size = 400
+    experiment_config.on_policy_minibatch_size = 400
 
-experiment_config.max_n_iters = 2 # epoch
-experiment_config.max_n_frames = 8_000_000
-experiment_config.loggers = ["csv"]
-experiment_config.create_json = True
-experiment_config.save_folder = "results"
-if torch.cuda.is_available():
-    print('running cuda')
-    experiment_config.sampling_device = 'cuda'
-    experiment_config.train_device = 'cuda'
-experiment_config.off_policy_train_batch_size = 400
-experiment_config.on_policy_minibatch_size = 400
+    # Create the experiment
+    # experiment = Experiment(
+    #     task=task,
+    #     algorithm_config=algorithm_config,
+    #     model_config=model_config,
+    #     critic_model_config=critic_model_config,
+    #     seed=0,
+    #     config=experiment_config,
+    # )
 
-# Create the experiment
-# experiment = Experiment(
-#     task=task,
-#     algorithm_config=algorithm_config,
-#     model_config=model_config,
-#     critic_model_config=critic_model_config,
-#     seed=0,
-#     config=experiment_config,
-# )
+    benchmark = Benchmark(
+        algorithm_configs=algorithm_configs,
+        model_configs=model_configs,
+        tasks=tasks,
+        seeds=seeds,
+        experiment_config=experiment_config,
+        num_process=4,
+        run_curriculum=True
+    )
 
-benchmark = Benchmark(
-    algorithm_configs=algorithm_configs,
-    model_configs=model_configs,
-    tasks=tasks,
-    seeds=seeds,
-    experiment_config=experiment_config,
-)
+    # benchmark.run_sequential()
+    benchmark.run_parallel()
 
-benchmark.run_sequential()
+    # Log experiment folder path
+    # print(experiment.name)
 
-# Log experiment folder path
-# print(experiment.name)
+    # Run experiment
+    # experiment.run()
+    print(f'benchmark took {time.time() - start_time} seconds in total')
 
-# Run experiment
-# experiment.run()
-print(f'benchmark took {time.time() - start_time} seconds in total')
+if __name__ == "__main__":
+    main()
--- src/custom_benchmark.py
+++ src/custom_benchmark.py
@@ -20,6 +20,9 @@ from src.BaselineVmasTask import BaselineVmasTask
 from utils.pt_export import convert_pt_to_gif
 
 
+from benchmarl.algorithms import MappoConfig
+from benchmarl.models.mlp import MlpConfig
+
 class Benchmark:
     """A benchmark.
 
@@ -44,6 +47,7 @@ class Benchmark:
         experiment_config: ExperimentConfig,
         critic_model_config: Optional[ModelConfig] = None,
         num_process = 1,
+        run_curriculum = False
     ):
         self.algorithm_configs = algorithm_configs
         self.tasks = tasks
@@ -55,6 +59,7 @@ class Benchmark:
         )
         self.experiment_config = experiment_config
         self.num_process = num_process
+        self.run_curriculum = run_curriculum
 
         print(f"Created benchmark with {self.n_experiments} experiments.")
 
@@ -62,6 +67,49 @@ class Benchmark:
     def n_experiments(self):
         """The number of experiments in the benchmark."""
         return len(self.model_configs) * len(self.algorithm_configs) * len(self.tasks) * len(self.seeds) + 1
+
+    def get_experiments_info(self):
+        """Yields one experiment at a time"""
+        for model_config in self.model_configs:
+            for algorithm_config in self.algorithm_configs:
+                for task in self.tasks:
+                    for seed in self.seeds:
+                        yield {
+                            'task': task,
+                            'algorithm_config': algorithm_config,
+                            'seed': seed,
+                            'model_config': model_config,
+                            'critic_model_config': self.critic_model_config,
+                            'config': self.experiment_config,
+                        }
+        
+        # Curriculum learning for baseline only
+        # ex. freeze_timeline = {
+        #     100: ["civilian", "policeHQ"],
+        #     1100: ["listener"],
+        #     2100: None,
+        # } for 3000 epochs total
+        if self.run_curriculum:
+            epochs = self.experiment_config.max_n_iters
+            speaker_freeze = int(0.03 * epochs)
+            listener_freeze = speaker_freeze + int(0.3 * epochs)
+            no_freeze = listener_freeze + int(0.3 * epochs)
+            freeze_timeline = {
+                speaker_freeze: ["civilian", "policeHQ"],
+                listener_freeze: ["listener"],
+                no_freeze: None,
+            }
+            print(f"curriculum freeze={freeze_timeline}")
+
+            yield {
+                'freeze_timeline': freeze_timeline,
+                'task': self.tasks[0],
+                'algorithm_config': self.algorithm_configs[0],
+                'seed': list(self.seeds)[0],
+                'model_config': self.model_configs[0],
+                'critic_model_config': self.critic_model_config,
+                'config': self.experiment_config,
+            }
 
     def get_experiments(self) -> Iterator[Experiment]:
         """Yields one experiment at a time"""
@@ -112,8 +160,77 @@ class Benchmark:
             "model": str(type(experiment.model_config)),
         }, indent=2))
     
+    def run_experiment(self, experiment):
+        """Wrapper function for running an experiment and converting its output."""
+        self.display_experiment(experiment=experiment)
+        try:
+            experiment.run()
+            start_time = time.time()
+            video_file_path = f'{experiment.config.save_folder}/{experiment.name}/{experiment.name}/videos'
+            convert_pt_to_gif(video_file_path)
+            print(f'video conversion took {time.time() - start_time} seconds')
+        except KeyboardInterrupt as interrupt:
+            print("\n\nBenchmark was closed gracefully\n\n")
+            experiment.close()
+            raise interrupt
+
+    def worker(self, experiment_queue):
+        """Worker function to process experiments from the queue."""
+        while True:
+            experiment_info = experiment_queue.get()  # Retrieve an experiment to run
+            if experiment_info is None:
+                # None is used as a signal to stop the worker
+                break
+            # if 'freeze_timeline' in experiment_info:
+            #     experiment = CustomExperiment(
+            #         freeze_timeline=experiment_info['freeze_timeline'],
+            #         task=experiment_info['tasks'],
+            #         algorithm_config=experiment_info['algorithm_configs'],
+            #         seed=experiment_info['seeds'],
+            #         model_config=experiment_info['model_configs'],
+            #         critic_model_config=experiment_info['critic_model_config'],
+            #         config=experiment_info['config'],
+            #     )
+            # else:
+            #     experiment = Experiment(
+            #         task=experiment_info['task'],
+            #         algorithm_config=experiment_info['algorithm_config'],
+            #         seed=experiment_info['seed'],
+            #         model_config=experiment_info['model_config'],
+            #         critic_model_config=experiment_info['critic_model_config'],
+            #         config=experiment_info['config'],
+            #     )
+            experiment = Experiment(
+                task=BaselineVmasTask.SIMPLE_SPEAKER_LISTENER.get_from_yaml(),
+                algorithm_config=experiment_info['algorithm_config'],
+                seed=experiment_info['seed'],
+                model_config=experiment_info['model_config'],
+                critic_model_config=experiment_info['critic_model_config'],
+                config=experiment_info['config'],
+            )
+            self.run_experiment(experiment)
+
     def run_parallel(self):
-        pass
+        """Run experiments in parallel, managing GPU resources and using torch multiprocessing."""
+        # Create a queue that will be used to feed experiments to the worker processes
+        experiment_queue = mp.Queue(maxsize=self.num_process)
+        
+        # Start worker processes
+        processes = [mp.Process(target=self.worker, args=(experiment_queue,)) for _ in range(self.num_process)]
+        for p in processes:
+            p.start()
+        
+        # Feed experiments into the queue
+        for experiment in self.get_experiments_info():
+            experiment_queue.put(experiment)
+        
+        # Signal workers to stop by adding 'None' to the queue for each worker
+        for _ in range(self.num_process):
+            experiment_queue.put(None)
+        
+        # Wait for all workers to finish
+        for p in processes:
+            p.join()
 
     def run_sequential(self):
         # experiments = self.get_experiments()
@@ -130,17 +247,5 @@ class Benchmark:
 
         """Run all the experiments in the benchmark in a sequence."""
         for i, experiment in enumerate(self.get_experiments()):
-            self.display_experiment(experiment)
             print(f"\nRunning experiment {i+1}/{self.n_experiments}.\n")
-
-            try:
-                experiment.run()
-                start_time = time.time()
-                video_file_path = f'{experiment.config.save_folder}/{experiment.name}/{experiment.name}/videos'
-                convert_pt_to_gif(video_file_path)
-                print(f'video conversion took {time.time() - start_time} seconds')
-            except KeyboardInterrupt as interrupt:
-                print("\n\nBenchmark was closed gracefully\n\n")
-                experiment.close()
-                raise interrupt
-
+            self.run_experiment(experiment=experiment)
